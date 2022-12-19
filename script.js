d10e3be7d9c482fba8b99cf66c0b16e96ff78d22
mampz-io <mkheer@ucdavis.edu>
2022-12-18 21:09:10 -0800

    Cart UI 90% finished

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
+
+//Mongo Connection credentials
+const CONNECTION_URL =
+  "mongodb+srv://luxe3sixty:chaishai1@cluster0.syd8n.mongodb.net/?retryWrites=true&w=majority";
+const DATABASE_NAME = process.env.DATABASE_NAME;
+const MongoClient = require("mongodb").MongoClient;
+let _db;
+module.exports = {
+  connectToServer: function (callback) {
+    MongoClient.connect(CONNECTION_URL, function (err, client) {
+      _db = client.db(DATABASE_NAME);
+      console.log("Connected to database: " + DATABASE_NAME);
+      return callback(err);
+    });
+  },
+  getDb: function () {
+    return _db;
+  },
+};
+
