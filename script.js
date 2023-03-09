b1f23b8c20b35ef548ca5dcb5a3161588e84b05d
Glitch (chaishaiboba) <none>
2023-03-09 00:05:54 +0000

    ⛈️🪁 Updated with Glitch

--- script.js
+++ script.js
@@ -7,27 +7,6 @@ function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
 
-//Mongo Connection credentials
-const CONNECTION_URL =
-  "mongodb+srv://luxe3sixty:chaishai1@cluster0.syd8n.mongodb.net/?retryWrites=true&w=majority";
-const DATABASE_NAME = process.env.DATABASE_NAME;
-const MongoClient = require("mongodb").MongoClient;
-let _db;
-module.exports = {
-  connectToServer: function (callback) {
-    MongoClient.connect(CONNECTION_URL, function (err, client) {
-      _db = client.db(DATABASE_NAME);
-      console.log("Connected to database: " + DATABASE_NAME);
-      return callback(err);
-    });
-  },
-  getDb: function () {
-    return _db;
-  },
-};
-
-
-
 //javascript for about page gallery 
 let slideIndex = 1;
 showSlides(slideIndex);
@@ -59,4 +38,3 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
-
